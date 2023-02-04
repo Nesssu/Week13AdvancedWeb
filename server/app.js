e93bd37a8e53c10ae03b31c63f29d0ed5e8220f8
Nesssu <80637080+Nesssu@users.noreply.github.com>
2023-02-04 15:21:27 +0200

    Part 5

--- server/app.js
+++ server/app.js
@@ -28,10 +28,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors({origin: "http://localhost:3000", optionsSuccessStatus: 200}))
-
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+if (process.env.NODE_ENV === "production")
+{
+    app.use(express.static(path.resolve("..", "client", "build")));
+    app.get("*", (req, res) =>
+    {
+        res.sendFile(path.resolve("..", "client", "build", "index.html"))
+    });
+}
+else if (process.env.NODE_ENV === "development")
+{
+    const corsOptions = {
+        origin: "http:localhost:3000",
+        optionsSuccessStatus: 200
+    };
+    app.use(cors(corsOptions));
+}
 
 module.exports = app;
